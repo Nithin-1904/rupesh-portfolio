22d31b4419e67e101b69f4625efc3508c1859bb4
Nithin Sarode <103872860+Nithin-1904@users.noreply.github.com>
2025-12-03 11:25:07 +0530

    feat: mobile sidebar and topbar fixes

--- src/app/(with-layout-pages)/components/MobileSideBar.tsx
+++ src/app/(with-layout-pages)/components/MobileSideBar.tsx
@@ -1,8 +1,112 @@
-import React from "react";
-import ThemeToggleButton from "~/shadcn/custom/theme-toggle-button";
+import React, { cloneElement, useState } from "react";
+import { usePathname } from "next/navigation";
+
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+
+import { LayoutPanelLeft } from "lucide-react";
+
+import Link from "next/link";
+
+import { cn } from "~/lib/utils";
+
+import { Button } from "~/shadcn/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "~/shadcn/ui/sheet";
+
+const SIDEBAR_LINKS = {
+  home: {
+    name: "Home",
+    href: "/home",
+    icon: <LayoutPanelLeft />,
+  },
+  people: {
+    name: "People",
+    href: "/people",
+    icon: <LayoutPanelLeft />,
+  },
+  publications: {
+    name: "Publications",
+    href: "/publications",
+    icon: <LayoutPanelLeft />,
+  },
+  teaching: {
+    name: "Teaching",
+    href: "/teaching",
+    icon: <LayoutPanelLeft />,
+  },
+  outreach: {
+    name: "Outreach",
+    href: "/outreach",
+    icon: <LayoutPanelLeft />,
+  },
+  gallery: {
+    name: "Gallery",
+    href: "/gallery",
+    icon: <LayoutPanelLeft />,
+  },
+};
 
 const MobileSideBar = () => {
-  return <ThemeToggleButton />;
+  const [isSheetOpen, setIsSheetOpen] = useState<boolean>(false);
+  const pathname = usePathname();
+
+  const isActive = (path: string) => pathname === path;
+  return (
+    <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          className={"mr-2 xl:hidden"}
+          onClick={() => {
+            setIsSheetOpen(true);
+          }}
+        >
+          <HamburgerMenuIcon className={"h-4 w-4"} />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={"left"} className="w-1/2 py-10">
+        <div className={"flex flex-col justify-center gap-2"}>
+          {Object.entries(SIDEBAR_LINKS).map(([_, sidebarLink]) => {
+            if (sidebarLink?.name) {
+              return (
+                <Link
+                  href={sidebarLink.href}
+                  key={sidebarLink.name}
+                  className={"m-0 w-full"}
+                >
+                  <Button
+                    variant={"secondary"}
+                    className={cn(
+                      "bg-background hover:bg-secondary w-full justify-start truncate border border-transparent px-2 shadow-none transition-all duration-150 ease-linear",
+                      isActive(sidebarLink?.href) &&
+                        "border-primary/10 bg-secondary text-primary"
+                    )}
+                    onClick={() => {
+                      setIsSheetOpen(false);
+                    }}
+                  >
+                    {sidebarLink.icon && (
+                      <span className={"shrink-0"}>
+                        {cloneElement(sidebarLink.icon, {
+                          className: cn(
+                            "h-5 w-5",
+                            isActive(sidebarLink?.href) && "stroke-current"
+                          ),
+                        })}
+                      </span>
+                    )}
+                    <span className="ml-2 duration-200">
+                      {sidebarLink.name}
+                    </span>
+                  </Button>
+                </Link>
+              );
+            }
+          })}
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileSideBar;
--- src/app/(with-layout-pages)/components/TopBar.tsx
+++ src/app/(with-layout-pages)/components/TopBar.tsx
@@ -2,8 +2,6 @@
 
 import { usePathname, useRouter } from "next/navigation";
 
-import { BatteryFull } from "lucide-react";
-
 import ThemeToggleButton from "~/shadcn/custom/theme-toggle-button";
 import { cn } from "~/lib/utils";
 import MobileSideBar from "./MobileSideBar";
@@ -26,78 +24,9 @@ const TopBar = () => {
   const isActive = (path: string) => pathname === path;
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b shadow-sm">
-      <div className="flex h-16 items-center px-4 justify-between sm:space-x-0 sm:px-4 sm:gap-10">
+      <div className="flex h-16 items-center px-4 justify-between sm:space-x-0 sm:px-4 gap-10">
         <MobileSideBar />
-        {/* <BatteryFull className="text-primary h-8 w-8" /> */}
-        {/* <div className="flex sm:gap-5 md:gap-10 items-center justify-center w-full">
-          <div className="flex items-center sm:gap-4 md:gap-6 lg:gap-10">
-            <nav
-              className={cn(
-                "hover:text-primary cursor-pointer font-semibold text-lg",
-                isActive("/home") && "text-primary"
-              )}
-              onClick={() => router.push("/home")}
-            >
-              Home
-            </nav>
-            <nav
-              className={cn(
-                "hover:text-primary cursor-pointer font-semibold text-lg",
-                isActive("/people") && "text-primary"
-              )}
-              onClick={() => router.push("/people")}
-            >
-              People
-            </nav>
-            <nav
-              className={cn(
-                "hover:text-primary cursor-pointer font-semibold text-lg",
-                isActive("/publications") && "text-primary"
-              )}
-              onClick={() => router.push("/publications")}
-            >
-              Publications
-            </nav>
-          </div>
-          <h1 className="lg:text-3xl md:text-2xl sm:text-xl font-extrabold sm:px-3 md:px-10">
-            RUPESH CHILLALE
-          </h1>
-          <div className="flex items-center sm:gap-4 md:gap-6 lg:gap-10">
-            <nav
-              className={cn(
-                "hover:text-primary cursor-pointer font-semibold text-lg",
-                isActive("/teaching") && "text-primary"
-              )}
-              onClick={() => router.push("/teaching")}
-            >
-              Teaching
-            </nav>
-            <nav
-              className={cn(
-                "hover:text-primary cursor-pointer font-semibold text-lg",
-                isActive("/outreach") && "text-primary"
-              )}
-              onClick={() => router.push("/outreach")}
-            >
-              Outreach
-            </nav>
-            <nav
-              className={cn(
-                "hover:text-primary cursor-pointer font-semibold text-lg",
-                isActive("/gallery") && "text-primary"
-              )}
-              onClick={() => router.push("/gallery")}
-            >
-              Gallery
-            </nav>
-          </div>
-        </div>
-        <div className="flex flex-1 items-center justify-end space-x-4 w-full">
-          <nav className="flex items-center gap-4">
-            <ThemeToggleButton />
-          </nav>
-        </div> */}
-        <div className="hidden md:flex gap-10 items-center">
+        <div className="hidden lg:flex gap-15 w-full justify-end items-center">
           {navItems.slice(0, 3).map((item) => (
             <nav
               key={item.path}
@@ -113,12 +42,15 @@ const TopBar = () => {
         </div>
 
         {/* Title */}
-        <h1 className="text-xl md:text-2xl lg:text-3xl font-extrabold text-center flex-1 md:px-10">
+        <h1
+          className="text-xl md:text-2xl whitespace-nowrap lg:text-3xl font-extrabold text-center px-5"
+          onClick={() => router.push("/home")}
+        >
           RUPESH CHILLALE
         </h1>
 
         {/* Right side links (Desktop Only) */}
-        <div className="hidden md:flex gap-10 items-center">
+        <div className="hidden lg:flex gap-15 w-full items-center">
           {navItems.slice(3).map((item) => (
             <nav
               key={item.path}
